e9de6d7cc173f178b5d1211a4e08a67bc7ba8a7b
gauti94 <gauti1994@gmail.com>
2025-09-11 20:24:02 +0000

    færist upp og niður

---
+++ spadi-orvar_files/spadi-orvar.js
@@ -0,0 +1,73 @@
+/////////////////////////////////////////////////////////////////
+//    Sýnidæmi í Tölvugrafík
+//     Sýnir notkun á lyklaborðsatburðum til að hreyfa spaða
+//
+//    Hjálmtýr Hafsteinsson, september 2025
+/////////////////////////////////////////////////////////////////
+var canvas;
+var gl;
+var ymove = 0.0;
+
+
+window.onload = function init() {
+
+    canvas = document.getElementById( "gl-canvas" );
+    
+    gl = WebGLUtils.setupWebGL( canvas );
+    if ( !gl ) { alert( "WebGL isn't available" ); }
+    
+    gl.viewport( 0, 0, canvas.width, canvas.height );
+    gl.clearColor( 0.8, 0.8, 0.8, 1.0 );
+
+    //
+    //  Load shaders and initialize attribute buffers
+    //
+    var program = initShaders( gl, "vertex-shader", "fragment-shader" );
+    gl.useProgram( program );
+    
+    var vertices = [
+        vec2( -0.2, -1.0 ),
+        vec2( 0.0, -0.6 ),
+        vec2( 0.2, -1.0 )
+    ];
+    
+    // Load the data into the GPU
+    var bufferId = gl.createBuffer();
+    gl.bindBuffer( gl.ARRAY_BUFFER, bufferId );
+    gl.bufferData( gl.ARRAY_BUFFER, flatten(vertices), gl.DYNAMIC_DRAW );
+
+    // Associate out shader variables with our data buffer
+    var vPosition = gl.getAttribLocation( program, "vPosition" );
+    gl.vertexAttribPointer( vPosition, 2, gl.FLOAT, false, 0, 0 );
+    gl.enableVertexAttribArray( vPosition );
+
+    // Event listener for keyboard
+    window.addEventListener("keydown", function(e){
+        switch( e.keyCode ) {
+            case 40:	// vinstri ör
+                ymove = -0.04;
+                break;
+            case 38:	// hægri ör
+                ymove = 0.04;
+                break;
+            default:
+                ymove = 0.0;
+        }
+        for(i=0; i<3; i++) {
+            vertices[i][1] += ymove;
+        }
+
+        gl.bufferSubData(gl.ARRAY_BUFFER, 0, flatten(vertices));
+    } );
+
+    render();
+}
+
+
+function render() {
+    
+    gl.clear( gl.COLOR_BUFFER_BIT );
+    gl.drawArrays( gl.TRIANGLES, 0, 3 );
+
+    window.requestAnimFrame(render);
+}
