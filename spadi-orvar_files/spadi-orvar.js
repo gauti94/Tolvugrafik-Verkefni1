2c136c3fde5371a0f595018ab2d7140aabb16141
gauti94 <gauti1994@gmail.com>
2025-09-12 23:24:05 +0000

    snúa við þríhyrning þegar hann fer efst og neðst

--- spadi-orvar_files/spadi-orvar.js
+++ spadi-orvar_files/spadi-orvar.js
@@ -6,86 +6,117 @@
 /////////////////////////////////////////////////////////////////
 var canvas;
 var gl;
-
+var theta = 0.5;
+var vertices = [vec2(-0.1, -0.95),
+    vec2(0.0, -0.65),
+    vec2(0.1, -0.95)];
+var atTop = false;
+var atBottom = false;
+var pointingUp = true;
+var pointingDown = false;
 
 window.onload = function init() {
+  canvas = document.getElementById("gl-canvas");
 
-    canvas = document.getElementById( "gl-canvas" );
-    
-    gl = WebGLUtils.setupWebGL( canvas );
-    if ( !gl ) { alert( "WebGL isn't available" ); }
-    
-    gl.viewport( 0, 0, canvas.width, canvas.height );
-    gl.clearColor( 0.8, 0.8, 0.8, 0.9 );
+  gl = WebGLUtils.setupWebGL(canvas);
+  if (!gl) {
+    alert("WebGL isn't available");
+  }
 
-    //
-    //  Load shaders and initialize attribute buffers
-    //
-    var program = initShaders( gl, "vertex-shader", "fragment-shader" );
-    gl.useProgram( program );
-    
-    var vertices = [
-        vec2( -0.1, -1.0 ),
-        vec2( 0.0, -0.85),
-        vec2( 0.1, -1.0)
-    ];
-    
-    // Load the data into the GPU
-    var bufferId = gl.createBuffer();
-    gl.bindBuffer( gl.ARRAY_BUFFER, bufferId );
-    gl.bufferData( gl.ARRAY_BUFFER, flatten(vertices), gl.DYNAMIC_DRAW );
+  gl.viewport(0, 0, canvas.width, canvas.height);
+  gl.clearColor(0.8, 0.8, 0.8, 0.9);
 
-    // Associate out shader variables with our data buffer
-    var vPosition = gl.getAttribLocation( program, "vPosition" );
-    gl.vertexAttribPointer( vPosition, 2, gl.FLOAT, false, 0, 0 );
-    gl.enableVertexAttribArray( vPosition );
+  //
+  //  Load shaders and initialize attribute buffers
+  //
+  var program = initShaders(gl, "vertex-shader", "fragment-shader");
+  gl.useProgram(program);
 
-    // Event listener for keyboard
-    window.addEventListener("keydown", function(e){
-        var newYMove = 0.0;
-        var newXMove = 0.0;
-        switch( e.keyCode ) {
-            case 40:	// niður ör
-                newYMove = -0.4;
-                break;
-            case 39: //hægri ör
-                newXMove = 0.25;
-                break;
-            case 38:	// upp ör
-                newYMove = 0.4;
-                break;
-            case 37: // vinstri ör
-                newXMove = -0.25;
-                break;
+  // Load the data into the GPU
+  var bufferId = gl.createBuffer();
+  gl.bindBuffer(gl.ARRAY_BUFFER, bufferId);
+  gl.bufferData(gl.ARRAY_BUFFER, flatten(vertices), gl.DYNAMIC_DRAW);
+
+  // Associate out shader variables with our data buffer
+  var vPosition = gl.getAttribLocation(program, "vPosition");
+  gl.vertexAttribPointer(vPosition, 2, gl.FLOAT, false, 0, 0);
+  gl.enableVertexAttribArray(vPosition);
+
+  // Event listener for keyboard
+  window.addEventListener("keydown", function (e) {
+    var newYMove = 0.0;
+    var newXMove = 0.0;
+    switch (e.keyCode) {
+      case 40: // niður ör
+        newYMove = -0.4;
+        break;
+      case 39: //hægri ör
+        newXMove = 0.25;
+        break;
+      case 38: // upp ör
+        newYMove = 0.4;
+        break;
+      case 37: // vinstri ör
+        newXMove = -0.25;
+        break;
+    }
+    var canMove = true;
+    for (i = 0; i < 3; i++) {
+      var newX = vertices[i][0] + newXMove;
+      console.log(newX);
+      var newY = vertices[i][1] + newYMove;
+      console.log(newY);
+      if (newX < -1.0 || newX > 1.0 || newY < -1.0 || newY > 1.0) {
+        canMove = false;
+        break;
+      }
+    }
+    if (canMove) {
+      for (i = 0; i < 3; i++) {
+        vertices[i][0] += newXMove;
+        vertices[i][1] += newYMove;
+      }
+      
+      if (vertices[1][1] >= 0.94) {
+        if (!atTop) {
+          atTop = true;
+          var tempY = vertices[1][1];
+          vertices[1][1] = vertices[0][1];
+          vertices[0][1] = tempY;
+          vertices[2][1] = tempY;
+          pointingDown = true;
+          pointingUp = false;
         }
-        var canMove = true;
-        for(i = 0; i < 3; i++) {
-            var newX = vertices[i][0] + newXMove;
-            var newY = vertices[i][1] + newYMove;
-            if (newX < -1.0 || newX > 1.0 || newY< -1.0 || newY> 1.0) {
-                canMove = false;
-                break;
-            }
+      } else {
+        atTop = false;
+      }
+      
+      if (vertices[1][1] <= -0.94) {
+        if (!atBottom) {
+          atBottom = true;
+          var tempY = vertices[1][1];
+          vertices[1][1] = vertices[0][1];
+          vertices[0][1] = tempY;
+          vertices[2][1] = tempY;
+          pointingUp = true;
+          pointingDown = false;
         }
-        if (canMove) {
-            for( i = 0; i < 3; i++) {
-                vertices[i][0] += newXMove;
-                vertices[i][1] += newYMove;
+      } else {
+        atBottom = false;
+      }
+      console.log("atTop:", atTop);
+      console.log("atBottom:", atBottom);
+    }
 
-            }
-        }
+    gl.bufferSubData(gl.ARRAY_BUFFER, 0, flatten(vertices));
+  });
 
-        gl.bufferSubData(gl.ARRAY_BUFFER, 0, flatten(vertices));
-    } );
-
-    render();
-}
-
+  render();
+};
 
 function render() {
-    
-    gl.clear( gl.COLOR_BUFFER_BIT );
-    gl.drawArrays( gl.TRIANGLES, 0, 3 );
+  gl.clear(gl.COLOR_BUFFER_BIT);
+  gl.drawArrays(gl.TRIANGLES, 0, 3);
 
-    window.requestAnimFrame(render);
+  window.requestAnimFrame(render);
 }
